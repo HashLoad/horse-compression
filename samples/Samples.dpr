26866aecf55230df764d62d7f96cba980be61d05
viniciussanchez <viniciuss.sanchez@gmail.com>
2019-08-28 20:05:02 -0300

    Added samples

---
+++ samples/Samples.dpr
@@ -0,0 +1,32 @@
+program Samples;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  Horse, Horse.Jhonson, Horse.Compression, System.JSON;
+
+var
+  App: THorse;
+
+begin
+  App := THorse.Create(9000);
+
+  App.Use(Compression);
+  App.Use(Jhonson);
+
+  App.Get('ping',
+    procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
+    var
+      I: Integer;
+      LPong: TJSONArray;
+    begin
+      LPong := TJSONArray.Create;
+      for I := 0 to 1000 do
+        LPong.Add(TJSONObject.Create(TJSONPair.Create('ping', 'pong')));
+      Res.Send(LPong);
+    end);
+
+  App.Start;
+end.
