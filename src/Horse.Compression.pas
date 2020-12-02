102b15513e79fa0efa53632531d95d0056f9c34d
Danilo Lucas <developer.dlio@gmail.com>
2020-12-02 07:47:16 -0300

    When there is an exception it should work

--- src/Horse.Compression.pas
+++ src/Horse.Compression.pas
@@ -1,6 +1,5 @@
 unit Horse.Compression;
 
-
 interface
 
 uses Horse, System.Classes, System.ZLib, System.SysUtils, Web.HTTPApp, System.JSON;
@@ -23,61 +22,68 @@ begin
 end;
 
 procedure Middleware(Req: THorseRequest; Res: THorseResponse; Next: TProc);
-type
-  TCompressionType = (ctDeflate, ctGZIP);
+  procedure _Exec(const AReq: THorseRequest; const ARes: THorseResponse);
+  type
+    TCompressionType = (ctDeflate, ctGZIP);
 
-const
-  COMPRESSION_TYPE_AS_STRING: array [TCompressionType] of string = ('deflate', 'gzip');
-  COMPRESSION_ZLIB_WINDOW_BITS: array [TCompressionType] of Integer = (-15, 31);
+  const
+    COMPRESSION_TYPE_AS_STRING: array [TCompressionType] of string = ('deflate', 'gzip');
+    COMPRESSION_ZLIB_WINDOW_BITS: array [TCompressionType] of Integer = (-15, 31);
 
-var
-  LMemoryStream: TMemoryStream;
-  LAcceptEncoding: string;
-  LZStream: TZCompressionStream;
-  LCompressionType: TCompressionType;
-  LWebResponse: TWebResponse;
-  LContent: string;
-begin
-  Next;
+  var
+    LMemoryStream: TMemoryStream;
+    LAcceptEncoding: string;
+    LZStream: TZCompressionStream;
+    LCompressionType: TCompressionType;
+    LWebResponse: TWebResponse;
+    LContent: string;
+  begin
+    LAcceptEncoding := AReq.Headers['accept-encoding'];
+    if LAcceptEncoding.Trim.IsEmpty then
+      Exit;
 
-  LAcceptEncoding := Req.Headers['accept-encoding'];
-  if LAcceptEncoding.Trim.IsEmpty then
-    Exit;
+    LContent := THorseHackResponse(ARes).GetWebResponse.Content;
+    if LContent.Trim.IsEmpty then
+      Exit;
 
-  LContent := THorseHackResponse(Res).GetWebResponse.Content;
-  if LContent.Trim.IsEmpty then
-    Exit;
+    LWebResponse := THorseHackResponse(Res).GetWebResponse;
+    LWebResponse.ContentStream := TStringStream.Create(LContent);
 
-  LWebResponse := THorseHackResponse(Res).GetWebResponse;
-  LWebResponse.ContentStream := TStringStream.Create(LContent);
+    if LWebResponse.ContentStream.Size <= CompressionThreshold then
+      Exit;
 
-  if LWebResponse.ContentStream.Size <= CompressionThreshold then
-    Exit;
+    if (Pos(COMPRESSION_TYPE_AS_STRING[TCompressionType.ctGZIP], LAcceptEncoding.Trim.ToLower) > 0) then
+      LCompressionType := TCompressionType.ctGZIP
+    else
+      LCompressionType := TCompressionType.ctDeflate;
 
-  if (Pos(COMPRESSION_TYPE_AS_STRING[TCompressionType.ctGZIP], LAcceptEncoding.Trim.ToLower) > 0) then
-    LCompressionType := TCompressionType.ctGZIP
-  else
-    LCompressionType := TCompressionType.ctDeflate;
-
-  LMemoryStream := TMemoryStream.Create;
-  try
-    LWebResponse.ContentStream.Position := 0;
-
-    LZStream := TZCompressionStream.Create(LMemoryStream, TZCompressionLevel.ZcMax, COMPRESSION_ZLIB_WINDOW_BITS[LCompressionType]);
+    LMemoryStream := TMemoryStream.Create;
     try
-      LZStream.CopyFrom(LWebResponse.ContentStream, 0);
+      LWebResponse.ContentStream.Position := 0;
+
+      LZStream := TZCompressionStream.Create(LMemoryStream, TZCompressionLevel.ZcMax, COMPRESSION_ZLIB_WINDOW_BITS[LCompressionType]);
+      try
+        LZStream.CopyFrom(LWebResponse.ContentStream, 0);
+      finally
+        FreeAndNil(LZStream);
+      end;
+
+      LMemoryStream.Position := 0;
+
+      LWebResponse.Content := EmptyStr;
+      LWebResponse.ContentStream.Size := 0;
+      LWebResponse.ContentStream.CopyFrom(LMemoryStream, 0);
+      LWebResponse.ContentEncoding := COMPRESSION_TYPE_AS_STRING[LCompressionType];
     finally
-      FreeAndNil(LZStream);
+      FreeAndNil(LMemoryStream);
     end;
+  end;
 
-    LMemoryStream.Position := 0;
-
-    LWebResponse.Content := EmptyStr;
-    LWebResponse.ContentStream.Size := 0;
-    LWebResponse.ContentStream.CopyFrom(LMemoryStream, 0);
-    LWebResponse.ContentEncoding := COMPRESSION_TYPE_AS_STRING[LCompressionType];
+begin
+  try
+    Next;
   finally
-    FreeAndNil(LMemoryStream);
+    _Exec(Req, Res);
   end;
 end;
 
